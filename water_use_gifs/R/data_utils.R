88adad26bf81c1d34dbf4369dcd4ce37d530a004
Jordan S Read <jread@usgs.gov>
2018-03-23 13:28:55 -0500

    adding data to dots

--- water_use_gifs/R/data_utils.R
+++ water_use_gifs/R/data_utils.R
@@ -2,26 +2,29 @@ get_dots <- function(json_file, proj.string, state_name){
   
   data <- read_json(json_file)$objects$foo$geometries
   
-  pt_coords <- matrix(data = rep(NA, length(data)*2), ncol = 2)
+  NA_out <- rep(NA, length(data))
+  
+  pt_coords <- matrix(data = c(NA_out, NA_out), ncol = 2)
+  
+  dot_data <- data.frame(total = NA_out, thermoelectric = NA_out, 
+                         publicsupply = NA_out, irrigation = NA_out, industrial = NA_out)
   for (j in seq_len(length(data))){
-    
-    coord <- data[[j]]$coordinates
-    state_abb <- data[[j]]$properties$STATE_ABBV
+    this_dot <- data[[j]]
+    coord <- this_dot$coordinates
+    state_abb <- this_dot$properties$STATE_ABBV
     if (state_abb == state_name){
       pt_coords[j, ] <- c(coord[[1]][1], coord[[2]][1])
     }
+    dot_data[j, ] <- this_dot$properties[names(dot_data)]
     
   }
   
   
   points <- pt_coords[!is.na(pt_coords[, 1]), ] %>% 
-      sp::SpatialPoints(proj4string = CRS("+proj=longlat +datum=WGS84")) %>% 
-      sp::spTransform(CRS(proj.string))
+    sp::SpatialPoints(proj4string = CRS("+proj=longlat +datum=WGS84")) %>% 
+    sp::spTransform(CRS(proj.string)) %>% 
+    sp::SpatialPointsDataFrame(data = dot_data[!is.na(pt_coords[, 1]), ])
   
-  # points <- cbind(locations$dec_long_va, locations$dec_lat_va) %>% 
-  #   sp::SpatialPoints(proj4string = CRS("+proj=longlat +datum=WGS84")) %>% 
-  #   sp::spTransform(CRS(proj.string)) %>% 
-  #   sp::SpatialPointsDataFrame(data = locations[c('site_no')])
-  # 
+  
   return(points)
 }
--- water_use_gifs/R/draw_utils.R
+++ water_use_gifs/R/draw_utils.R
@@ -16,14 +16,24 @@ plot_state_map <- function(state_sp, county_sp, dots_sp, metadata, watermark_fil
   
   png(filename, width = metadata[1], height = metadata[2], res=metadata[3], units = 'in')
   
-  par(mai=c(0,0,0,0), omi=c(0,0,0,0), xaxs = 'i', yaxs = 'i')
+  par(mai=c(0,0,0,0), omi=c(0,0,0,0)) #, xaxs = 'i', yaxs = 'i'
   
   plot(county_sp, col = NA, border = "grey60")
-  plot(state_sp, col = NA, border = "black", add = TRUE)
+  plot(state_sp, col = NA, border = "black", lwd = 1.2, add = TRUE)
   
   plot(dots_sp, add = TRUE, col = 'red', pch = 20, cex = 3)
+  dot_to_circle(dots_sp)
+  add_watermark(watermark_file)
   
   
+  dev.off()
+}
+
+dot_to_circle <- function(dots){
+  dots_sp
+}
+
+add_watermark <- function(watermark_file, ...){
   # --- watermark ---
   coord_space <- par()$usr
   watermark_alpha <- 0.4
@@ -31,14 +41,14 @@ plot_state_map <- function(state_sp, county_sp, dots_sp, metadata, watermark_fil
   
   which_image <- d[,,4] != 0 # transparency
   d[which_image] <- watermark_alpha
-  img_scale <- 200
+  img_scale <- 160
   
-  img_bump <- 20000
+  img_bump <- 10000
   
   x1 <- coord_space[1]+img_bump
   y1 <- coord_space[3]+img_bump
   
   rasterImage(d, x1, y1, x1+ncol(d)*img_scale, y1+nrow(d)*img_scale)
   
-  dev.off()
+  text(coord_space[2], coord_space[3]+img_bump*2, 'https://owi.usgs.gov/vizlab/water-use-15/', pos = 2, cex = 0.8)
 }
