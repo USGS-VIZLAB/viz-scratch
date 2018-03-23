87bb8e31ee779acb4db8ce824f01387d51a2b226
Jordan S Read <jread@usgs.gov>
2018-03-23 15:11:17 -0500

    adding pies

--- water_use_gifs/R/draw_utils.R
+++ water_use_gifs/R/draw_utils.R
@@ -21,7 +21,7 @@ plot_state_map <- function(state_sp, county_sp, dots_sp, metadata, watermark_fil
   plot(county_sp, col = "grey97", border = "grey70")
   plot(state_sp, col = NA, border = "black", lwd = 1.2, add = TRUE)
   
-  dot_to_circle(dots_sp)
+  dot_to_pie(dots_sp)
   add_watermark(watermark_file)
   
   
@@ -40,22 +40,74 @@ dot_to_circle <- function(dots){
   
 }
 
-add_watermark <- function(watermark_file, ...){
+dot_to_pie <- function(dots){
+  
+  scale_const <- 900
+  
+  categories <- c("irrigation", "industrial", "thermoelectric", "publicsupply")
+  cols <- c("irrigation" = "#59a14f", "industrial"="#e15759", 
+            "thermoelectric"="#edc948", "publicsupply"="#76b7b2", "other"="#A9A9A9")
+  
+  for (j in seq_len(length(dots))){
+    
+    transparency_alpha <- 'CC'
+    dot <- dots[j, ]
+    r <- sqrt(dot$total) * scale_const
+    
+    c.x <- dot@coords[1]
+    c.y <- dot@coords[2]
+    
+    #stole code from water-use-15
+    for (cat in categories){
+      cat_angle <- dot[[cat]] / dot[['total']]*2*pi
+      if (cat == head(categories, 1L)){
+        # start the first category mirror relative to the top
+        angle_from <- pi/2 - cat_angle/2
+        orig_ang <- angle_from
+      } else {
+        angle_from <- angle_to
+      }
+      angle_to <- angle_from + cat_angle
+      if (!is.na(cat_angle) & cat_angle > 0.01){
+        segments <- make_arc(c.x, c.y, r = r, angle_from, angle_to)
+        polygon(c(c.x, segments$x, c.x), c(c.y, segments$y, c.y), 
+                border = NA,
+                col = paste0(cols[[cat]],transparency_alpha))
+        lines(segments$x, segments$y, lwd=0.75, col = cols[[cat]])
+      }
+    }
+    if (!is.na(r) & cat == tail(categories, 1L) & angle_to < 2*pi + orig_ang){
+      
+      segments <- make_arc(c.x, c.y, r = r, angle_to, 2*pi + orig_ang)
+      polygon(c(c.x, segments$x, c.x), c(c.y, segments$y, c.y), 
+              border = NA,
+              col = paste0(cols[['other']],transparency_alpha))
+      lines(segments$x, segments$y, lwd=0.75, col = cols[['other']])
+    }
+  }
+}
+
+add_watermark <- function(watermark_file,...){
   # --- watermark ---
+  watermark_frac <- 0.2 # fraction of the width of the figure
+  watermark_bump_frac <- 0.01
   coord_space <- par()$usr
+  
   watermark_alpha <- 0.4
   d <- png::readPNG(watermark_file)
   
   which_image <- d[,,4] != 0 # transparency
   d[which_image] <- watermark_alpha
-  img_scale <- 160
   
-  img_bump <- 10000
+  coord_width <- coord_space[2]-coord_space[1]
+  coord_height <- coord_space[4]-coord_space[3]
+  watermark_width <- dim(d)[2]
+  img_scale <- coord_width*watermark_frac/watermark_width
   
-  x1 <- coord_space[1]+img_bump
-  y1 <- coord_space[3]+img_bump
+  x1 <- coord_space[1]+coord_width*watermark_bump_frac
+  y1 <- coord_space[3]+coord_height*watermark_bump_frac
   
   rasterImage(d, x1, y1, x1+ncol(d)*img_scale, y1+nrow(d)*img_scale)
   
-  text(coord_space[2], coord_space[3]+img_bump*2, 'https://owi.usgs.gov/vizlab/water-use-15/', pos = 2, cex = 0.8, col = 'grey50')
+  text(coord_space[2], y1+coord_height*watermark_bump_frac, 'https://owi.usgs.gov/vizlab/water-use-15/', pos = 2, cex = 0.8, col = 'grey50')
 }
