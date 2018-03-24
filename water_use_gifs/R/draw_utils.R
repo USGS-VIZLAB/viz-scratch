52dfd361f703203b3ae16d1330eb0987ee61d7f7
Jordan S Read <jread@usgs.gov>
2018-03-24 03:06:39 -0500

    gifs w/ pie transitions

--- water_use_gifs/R/draw_utils.R
+++ water_use_gifs/R/draw_utils.R
@@ -8,19 +8,80 @@ make_arc <- function(x0, y0, r, from_angle, to_angle){
   return(list(x = x_out, y = y_out))
 }
 
-
-plot_state_map <- function(state_sp, county_sp, dots_sp, metadata, watermark_file, filename){
+plot_dot_map <- function(state_sp, county_sp, watermark_file){
   
-  png(filename, width = metadata[1], height = metadata[2], res=metadata[3], units = 'in')
   
   par(mai=c(0,0,0,0), omi=c(0,0,0,0)) #, xaxs = 'i', yaxs = 'i'
   
   plot(county_sp, col = "grey97", border = "grey70")
   plot(state_sp, col = NA, border = "black", lwd = 1.2, add = TRUE)
-  
-  dot_to_pie(dots_sp)
   add_watermark(watermark_file)
+}
+
+cat_col <- function(cat){
+  cols <- c("irrigation" = "#59a14f", "industrial"="#e15759", 
+            "thermoelectric"="#edc948", "publicsupply"="#76b7b2", "other"="#A9A9A9")
+  cols[[cat]]
+}
+plot_dot_transitions <- function(state_sp, county_sp, dots_sp, metadata, watermark_file, filename, cat1, cat2, frames = 5){
   
+  col1 <- cat_col(cat1)
+  col2 <- cat_col(cat2)
+  cols <- colorRampPalette(c(col1, col2))(frames)
+  interp_vals <- seq(1,0, length.out = frames)
+  for (i in 1:frames){
+    file_parts <- strsplit(filename, '[.]')[[1]]
+    this_filename <- paste0(substr(file_parts[1], start = 1, stop = nchar(file_parts[1])-1), 
+                            i-1, '.', file_parts[2]) # hack alert!
+    
+    png(this_filename, width = metadata[1], height = metadata[2], res=metadata[3], units = 'in')
+    plot_dot_map(state_sp, county_sp, watermark_file)
+    tmp_dots <- dots_sp
+    tmp_dots[[cat1]] <- (interp_vals[i]*sqrt(tmp_dots[[cat1]])+ (1 - interp_vals[i])*sqrt(tmp_dots[[cat2]]))^2
+    dot_to_circle(tmp_dots, cat1, cols[i])
+    dev.off()
+  }
+  
+  dev.off()
+}
+
+plot_pie_transitions <- function(state_sp, county_sp, dots_sp, metadata, watermark_file, filename, cat_to, frames = 5){
+  
+  
+  interp_vals <- seq(1,0, length.out = frames)
+  all_cats <- c("irrigation", "industrial", "thermoelectric", "publicsupply")
+  cat_aways <- all_cats[!all_cats %in% cat_to]
+  
+  for (i in 1:frames){
+    
+    file_parts <- strsplit(filename, '[.]')[[1]]
+    this_filename <- paste0(substr(file_parts[1], start = 1, stop = nchar(file_parts[1])-1), 
+                            i-1, '.', file_parts[2]) # hack alert!
+    
+    png(this_filename, width = metadata[1], height = metadata[2], res=metadata[3], units = 'in')
+    plot_dot_map(state_sp, county_sp, watermark_file)
+    tmp_dots <- dots_sp
+    
+    total_now <- (interp_vals[i]*sqrt(tmp_dots$total)+ (1 - interp_vals[i])*sqrt(tmp_dots[[cat_to]]))^2
+    total_diff <- dots_sp$total - (total_now - dots_sp[[cat_to]])
+    
+    
+    tmp_dots$total <- total_now
+    for (cat in cat_aways){
+      orig_slice <- dots_sp$total - dots_sp[[cat_to]]
+      new_slice <- total_now - dots_sp[[cat_to]]
+      slice_frac <- dots_sp[[cat]]/(dots_sp$total-dots_sp[[cat_to]])
+      tmp_dots[[cat]] <- dots_sp[[cat]] - (orig_slice - new_slice)*slice_frac
+    }
+    
+    if (i == frames){
+      dot_to_circle(dots_sp, cat_to, border = cat_col(cat_to))
+    } else {
+      dot_to_pie(tmp_dots)
+    }
+    
+    dev.off()
+  }
   
   dev.off()
 }
@@ -41,9 +102,7 @@ dot_to_pie <- function(dots){
   
   scale_const <- 900
   
-  categories <- c("irrigation", "industrial", "thermoelectric", "publicsupply")
-  cols <- c("irrigation" = "#59a14f", "industrial"="#e15759", 
-            "thermoelectric"="#edc948", "publicsupply"="#76b7b2", "other"="#A9A9A9")
+  categories <- c("irrigation","industrial", "thermoelectric", "publicsupply")
   
   for (j in seq_len(length(dots))){
     
@@ -69,8 +128,8 @@ dot_to_pie <- function(dots){
         segments <- make_arc(c.x, c.y, r = r, angle_from, angle_to)
         polygon(c(c.x, segments$x, c.x), c(c.y, segments$y, c.y), 
                 border = NA,
-                col = paste0(cols[[cat]],transparency_alpha))
-        lines(segments$x, segments$y, lwd=0.75, col = cols[[cat]])
+                col = paste0(cat_col(cat),transparency_alpha))
+        lines(segments$x, segments$y, lwd=0.75, col = cat_col(cat))
       }
     }
     if (!is.na(r) & cat == tail(categories, 1L) & angle_to < 2*pi + orig_ang){
@@ -78,8 +137,8 @@ dot_to_pie <- function(dots){
       segments <- make_arc(c.x, c.y, r = r, angle_to, 2*pi + orig_ang)
       polygon(c(c.x, segments$x, c.x), c(c.y, segments$y, c.y), 
               border = NA,
-              col = paste0(cols[['other']],transparency_alpha))
-      lines(segments$x, segments$y, lwd=0.75, col = cols[['other']])
+              col = paste0(cat_col('other'),transparency_alpha))
+      lines(segments$x, segments$y, lwd=0.75, col = cat_col('other'))
     }
   }
 }
