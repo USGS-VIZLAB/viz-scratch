c41619979bcc129b56fca338170ac621088ae413
David Blodgett <dblodgett@usgs.gov>
2018-10-30 06:23:32 -0500

    Tuesday Morning

--- gage_outages/priority_map.R
+++ gage_outages/priority_map.R
@@ -204,7 +204,7 @@ gsMap <- ggplot() +
         plot.subtitle = element_text(hjust = 0.5)) +
   ggtitle(label = paste("Site Outage Summary", Sys.time()), subtitle = paste(nrow(siteInfo), "sites currently impacted")) +
   guides(color = guide_legend(title="Priority", order = 1)) + 
-  labs(caption = "         Quantitative Precipitation Forecast (QPF) VALID: 12Z 2018-10-29 THRU 12Z 2018-01-05\n")
+  labs(caption = "         Quantitative Precipitation Forecast (QPF) VALID: 12Z 2018-10-30 THRU 12Z 2018-01-06\n")
 
 gsMap
 ggsave(gsMap, filename = "site_outages_priority.pdf", width = 11, height = 7)
