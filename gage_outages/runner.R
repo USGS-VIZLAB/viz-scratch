570c02273e6cc11fefb554db9389c1f9e0674606
David Blodgett <dblodgett@usgs.gov>
2018-11-05 06:59:28 -0600

    mnonday update

--- gage_outages/runner.R
+++ gage_outages/runner.R
@@ -5,11 +5,11 @@ token <- gs_auth(cache = FALSE)
 title_2 <- gs_title("GOES/DA ISSUE STARTING 2018-10-20")
 current_site_list_raw <- gs_read(title_2, ws = "Gages", range = "A5:AC1000")
 
-latest_m_flows <- "max_flows_2018-11-04T00Z.rds"
+latest_m_flows <- "max_flows_2018-11-05T00Z.rds"
 
-qpf_caption <- "Quantitative Precipitation Forecast (QPF) Valid: 12Z 2018-11-04 Thru 12Z 2018-11-11\n"
+qpf_caption <- "Quantitative Precipitation Forecast (QPF) Valid: 12Z 2018-11-05 Thru 12Z 2018-11-12\n"
 
-nwm_caption <-  paste0(qpf_caption, "NWM forecasts from 00Z 11-04")
+nwm_caption <-  paste0(qpf_caption, "NWM forecasts from 00Z 11-05")
 
 source("../viz-scratch/gage_outages/outage_maps.R")
 source("../viz-scratch/gage_outages/priority_map.R")
