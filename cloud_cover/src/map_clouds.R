1a09ec7f81e493871b51cacc029653b64042c839
Platt <lplatt@usgs.gov>
2021-01-25 10:33:32 -0600

    add cloud moisture + include code to make version with gages, too

--- cloud_cover/src/map_clouds.R
+++ cloud_cover/src/map_clouds.R
@@ -2,10 +2,20 @@ map_clouds <- function(target_name, date, conus_sf, clouds_raster_fn, cloud_col,
   
   cloud_cover <- readRDS(clouds_raster_fn)
   
-  png(target_name, width = frame_width, height = frame_height)
-  plot(st_geometry(conus_sf))
+  png(target_name, width = frame_width, height = frame_height, type = "cairo")
+  par(omi = c(0,0,0,0), mai = c(0,0,0,0))
+  plot(st_geometry(conus_sf), xaxs = 'i', yaxs = 'i')
+  
+  # Add after conus_sf is plot, but before clouds (~ line 6-7) 
+  this_date_sites <- readRDS("in/site_loc_ready.rds") %>% 
+    left_join(filter(readRDS("in/site_data_ready.rds"), 
+                     Date == as.Date(date)), by = "site_no")
+  plot(st_geometry(this_date_sites), add = TRUE, bg = this_date_sites$bg, 
+       pch = this_date_sites$pch, cex = this_date_sites$cex, 
+       col = this_date_sites$col, lwd = this_date_sites$lwd)
+  
   plot(cloud_cover, add=TRUE, col = cloud_col, legend = FALSE)
-  title(sprintf("Cloud cover for %s", date), cex.main = 2.5)
+  title(sprintf("Cloud cover for %s", date), cex.main = 2.5, line = -4)
   dev.off()
   
 }
