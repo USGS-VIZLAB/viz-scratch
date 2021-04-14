5c01c719e89f6b65702444f51fbe2f16b0e5bf3a
coll nell <collnellphd@gmail.com>
2021-04-14 11:07:17 -0500

    use gganimate for gif generation

--- fire_area/scrape_fire_data.R
+++ fire_area/scrape_fire_data.R
@@ -11,7 +11,6 @@ fire_cost <- read_html('https://www.nifc.gov/fire-information/statistics/suppres
   filter(!is.na(Year))  # remove total rows
 fire_cost
 
-
 # plot wildfire area by cost ----------------------------------------------
 
 library(scico);library(scales)
@@ -58,50 +57,14 @@ fire_cost %>%
 
 ggsave("wilfire_acres_by_dollars.png", width = 16, height = 9)
 
-## make png for each year to animate
-years <- cost_df$Year
-
-
-plot_fire_year <- function(year){
-  cost_df %>%
-    filter(Year <= years[year]) %>%
-    ggplot(aes(Acres, Total, color = Acres))+
-    geom_point(aes(size=Fires), shape=16)+
-    geom_text(aes(label=Year, y=Total+140000000, x=Acres+350000), color="white")+
-    geom_text(data=labs, aes(label=lab), color="grey90", fontface="italic", size=4.5)+
-    theme_fire_dark()+
-    scale_size(range=c(2,8), limits=range(cost_df$Fires),
-               labels = scales::label_comma(), 
-               breaks=breaks_pretty(n=6))+
-    scale_color_scico(palette="lajolla", end=0.7, limits=range(cost_df$Acres))+
-    labs(x="", y="")+
-    scale_y_continuous(labels=scales::dollar_format(scale=1/1000000, suffix = "M"))+ # in millions of dollars
-    scale_x_continuous(labels=scales::label_number(scale=1/1000000, suffix = "M"), 
-                       limits=c(1000000, 10000000))+
-    ggtitle("Millions of acres burned &\nmillions of dollars spent suppressing wildfires")+
-    guides(size=FALSE,color=FALSE)
-  
-  
-  ggsave(sprintf("fire/wilfire_year_%s.png",year))
-  
-  
-}
-
-for (year in 1:length(years)){
-  plot_fire_year(year)
-}               
+# animate through time ----------------------------------------------------
 
 library(gganimate)
 
-cost_long <- NULL
-for (year in years){
-  yr_df <- cost_df %>% filter(Year <= year) %>% mutate(state = year)
-  cost_long <- rbind(cost_long, yr_df)
-}
-
-fire_ani <- fire_cost %>% mutate(state = "all") %>%
+fire_ani <- fire_cost %>% 
   ggplot(aes(Acres, Total, color = Acres))+
-  geom_point(aes(size=Fires, group = state), shape=16)+
+  geom_point(aes(size=Fires), shape=16)+
+  geom_text(aes(label="{Year}", y=3100000000, x=10000000), size=8, color="white")+
   geom_text(data=labs, aes(label=lab), color="grey90", fontface="italic", size=4.5)+
   theme_fire_dark()+
   scale_size(range=c(2,8), 
@@ -111,15 +74,15 @@ fire_ani <- fire_cost %>% mutate(state = "all") %>%
   labs(x="", y="")+
   scale_y_continuous(labels=scales::dollar_format(scale=1/1000000, suffix = "M"))+ # in millions of dollars
   scale_x_continuous(labels=scales::label_number(scale=1/1000000, suffix = "M"))+
-  ggtitle("Millions of acres burned &\nmillions of dollars spent supressing wildfirespressing wildfires                                {Year}")+
+  ggtitle("Millions of acres burned &\nmillions of dollars spent suppressing wildfires ")+
   guides(size=FALSE,color=FALSE)+
   transition_states(
-    state,
+    Year,
     transition_length = 2,
     state_length = 2
   ) +
   enter_fade() +
   shadow_mark()
 
-animate(fire_ani, width=1200, height = 675)
+animate(fire_ani, width=800, height = 450)
 anim_save("fire_animated_time.gif")
