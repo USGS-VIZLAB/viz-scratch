149f29af6783ade9a2374475bc98bd24d652dd5e
Platt <lplatt@usgs.gov>
2022-04-28 08:12:43 -0500

    had to update specify columns, otherwise just got Dry & Drier

--- flow_cartogram/src/prep_data.R
+++ flow_cartogram/src/prep_data.R
@@ -42,10 +42,11 @@ flow_by_day_by_state <- function(data_in, dv_site, sites_state) {
   data_in  %>%
     left_join(dv_site) %>% # adds state info for each gage
     group_by(state_cd, date, cond) %>% # aggregate by state, day, flow condition
-    summarize(n_gage = length(unique(site_no)))  %>% 
+    summarize(n_gage = length(unique(site_no)))  %>%
     left_join(sites_state) %>% # add total_gage
     mutate(prop = n_gage/total_gage) %>% # proportion of gages
     pivot_wider(id_cols = !n_gage, names_from = cond, values_from = prop, values_fill = 0) %>% # complete data for timepoints with 0 gages
-    pivot_longer(cols = Dry:Drier, names_to = "cond", values_to = "prop")
+    pivot_longer(cols = c("Normal", "Wet", "Wettest", "Driest", "Drier", "Dry"), 
+                 names_to = "cond", values_to = "prop")
 }
 
